4c4ec25f36051457498acbad1e0eb09c91dc3ba6
Quimy <bartekgier@interia.eu>
2017-06-12 18:37:02 +0200

    da

--- src/main/java/QAPlatform/web/HomeController.java
+++ src/main/java/QAPlatform/web/HomeController.java
@@ -27,10 +27,11 @@ public class HomeController {
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Model model){
+		
 		List<Question> questions = null;
 		questions = questionService.getAllQuestions();
-		System.out.println(questions.toString());
 		model.addAttribute("AllQuestions", questions);
 		return "home";
+		
 	}
 }
